e1d55c62182b979c7ba9e882b9658bba8895c1e3
utin-francis-peter <u.francispeter@gmail.com>
2023-05-05 16:16:56 +0100

    fix: updated href attr.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
           <nav>
             <ul>
               <li>
-                <a href="#">nav</a>
+                <a href="/">nav</a>
               </li>
             </ul>
           </nav>
